377529619195fa4a6cc5f35d7350042014da928e
Dmytro Lohush <dmytro.lohush@gmail.com>
2015-11-12 15:42:06 +0200

    created mainScreen interfaces

---
+++ CookBook/CacheProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CacheDelegate.swift
+//  CookBook
+//
+//  Created by Dmytro Lohush on 11/11/15.
+//  Copyright © 2015 integer. All rights reserved.
+//
+
+import Foundation
+
+protocol CacheProtocol {
+  func cacheData()
+  
+  func getDataFromCache()
+}
